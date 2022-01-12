22bacb6a44f4c38ebf9a8816e50742b73287f4a4
Jonas Ward Van den Bulcke <43600893+JonasWard@users.noreply.github.com>
2022-01-12 22:32:37 +0100

    try-out with hashrouter

--- src/App.js
+++ src/App.js
@@ -43,15 +43,17 @@ class App extends Component {
     render() {
         return (
             <HashRouter basename="/react-play">
-                <div className="App">
-                    <Header data={this.state.resumeData.main}/>
-                    <Opening data={this.state.resumeData.main}/>
-                    <About data={this.state.resumeData.main}/>
-                    <Resume data={this.state.resumeData.resume}/>
-                    <Portfolio data={this.state.resumeData.portfolio}/>
-                    <Contact data={this.state.resumeData.main}/>
-                    <Footer data={this.state.resumeData.main}/>
-                </div>
+                <link to="/">
+                    <div className="App">
+                        <Header data={this.state.resumeData.main}/>
+                        <Opening data={this.state.resumeData.main}/>
+                        <About data={this.state.resumeData.main}/>
+                        <Resume data={this.state.resumeData.resume}/>
+                        <Portfolio data={this.state.resumeData.portfolio}/>
+                        <Contact data={this.state.resumeData.main}/>
+                        <Footer data={this.state.resumeData.main}/>
+                    </div>
+                </link>
             </HashRouter>
         );
     }
